5be7adcf92ed4b884717837d6b8b389dcfd20d37
Aaron Acerboni <a.acerboni@gmail.com>
2012-06-15 13:43:44 +0100

    Use app.js pattern. Convert mixed spaced tabs / real tabs to only 2 space tabs. Remove unneeded _.mixin. Remove unneeded variable master.

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
-var http     = require('http'),
-	_ 		 = require('underscore')._,
-	globals  = require('./globals.js'),
-	graphMod = require('./grapher.js');
+var app      = require('http').createServer(handler),
+    _        = require('underscore')._,
+    globals  = require('./globals.js'),
+    graphMod = require('./grapher.js');
 
-_.mixin( require('underscore.deferred') );
+function handler(req, res) {
+  var rtn = [], grapher;
 
-http.createServer(function (req, res) {
-	var rtn = [], master, grapher;
+  if (req.url === '/favicon.ico') {
+    return;
+  }
 
-	if (req.url === '/favicon.ico') {
-		return;
-	}
+  if (req.url === '/') {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('<p>Type in a domain and path into the address bar e.g.</p>'+
+      '<pre>http://thiswebsite.com/twitter.com/ac94</pre>' +
+      '<p>The result will be returned as JSON</p>');
+    return;
+  }
 
-	if (req.url === '/') {
-		res.writeHead(200, {'Content-Type': 'text/html'});
-		res.end('<p>Type in a domain and path into the address bar e.g.</p>'+
-			'<pre>http://thiswebsite.com/twitter.com/ac94</pre>' +
-			'<p>The result will be returned as JSON</p>');
-		return;
-	}
+  grapher = new graphMod.Grapher();
 
-	grapher = new graphMod.Grapher();
+  grapher.buildGraph('http:/'+req.url).then(function(obj) {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify(obj));
+  });
+}
 
-	grapher.buildGraph('http:/'+req.url).then(function(obj) {
-		res.writeHead(200, {'Content-Type': 'application/json'});
-		res.end(JSON.stringify(obj));
-	});
+app.listen(globals.APP_PORT, globals.APP_IP);
 
-}).listen(1337, '127.0.0.1');
-
-console.log('Server running at http://127.0.0.1:1337/');
+console.log('App @ http://' + globals.APP_IP + ':' + globals.APP_PORT);
